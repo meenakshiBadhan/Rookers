6405c82d9d7591b923deaf008ae0bd024dd5eb44
Meenakshi <minileo1@gmail.com>
2013-12-02 10:58:16 +0530

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+  before_filter :current_user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
   before_save { self.email.downcase!}#|user| user.email = email.downcase }
-
+  before_save :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
@@ -22,4 +22,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
 
   has_secure_password
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+	end
 end
+
